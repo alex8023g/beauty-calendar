73b16f8d44840d26bb439e81af2c94c189d75828
alex8023g <alex8023@yandex.ru>
2025-11-09 13:19:26 +0300

    add scrollIntoView today calendar

--- src/components/DayBtn.tsx
+++ src/components/DayBtn.tsx
@@ -9,18 +9,26 @@ import { DaySchedule } from '@/components/DaySchedule';
 import type { Day } from '@/lib/createYearCalendar';
 import dayjs from 'dayjs';
 import { useEventsStore } from '@/store/store';
+import { useEffect, useRef } from 'react';
+
+const todayDateString = dayjs().format('YYYY-MM-DD');
 
 export function DayBtn({ day }: { day: Day }) {
   const events = useEventsStore((state) => state.events);
+  const el = useRef<HTMLButtonElement | null>(null);
+  useEffect(() => {
+    if (todayDateString === day.dateString) {
+      el.current?.scrollIntoView({ block: 'center' });
+    }
+  }, []);
   return (
     <Sheet>
       <SheetTrigger asChild>
         <button
           key={day.dateString}
+          ref={el}
           type='button'
-          data-is-today={
-            dayjs().format('YYYY-MM-DD') === day.dateString ? '' : undefined
-          }
+          data-is-today={todayDateString === day.dateString ? '' : undefined}
           // data-is-current-month={day?.monthDay ? '' : undefined}
           data-is-current-month=''
           className='relative bg-gray-50 py-1.5 text-gray-400 first:rounded-tl-lg last:rounded-br-lg hover:bg-gray-100 focus:z-10 data-is-current-month:bg-white data-is-current-month:text-gray-900 data-is-current-month:hover:bg-gray-100 nth-36:rounded-bl-lg nth-7:rounded-tr-lg dark:bg-gray-900/75 dark:text-gray-500 dark:hover:bg-gray-900/25 dark:data-is-current-month:bg-gray-900 dark:data-is-current-month:text-gray-100 dark:data-is-current-month:hover:bg-gray-900/50'
