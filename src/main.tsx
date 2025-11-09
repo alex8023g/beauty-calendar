4e010c27138414f631859542e2fa572f76eaa34c
alex8023g <alex8023@yandex.ru>
2025-11-09 14:16:04 +0300

    add notifications from capacitor

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,18 @@ import { BrowserRouter, Route, Routes } from 'react-router';
 import { HomePage } from './pages/HomePage.tsx';
 import { Preferences } from '@capacitor/preferences';
 import { useEventsStore } from './store/store.ts';
+import {
+  requestNotificationPermissions,
+  scheduleBasicNotification,
+} from './lib/notifications.ts';
 
 const { value } = await Preferences.get({ key: 'events' });
 if (value) {
   useEventsStore.setState({ events: JSON.parse(value) });
 }
+
+requestNotificationPermissions();
+scheduleBasicNotification();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
